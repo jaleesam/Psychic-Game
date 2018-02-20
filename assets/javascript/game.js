835a804fe12f800398db1b010299741470fc0f4d
Jaleesa McCrea <jaleesa@Jaleesas-MacBook-Pro.local>
2018-02-20 15:09:12 -0500

    linked files together, computer chooses random letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,9 @@
-<script>
-</script>
+
+//Computer's list of alphabet letters
+var alphabetLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    
+ //Computer guesses alphabet letter and stores that letter
+var letterTBG = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
+console.log(letterTBG);
+
+//Computer listens for key event
